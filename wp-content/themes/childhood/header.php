380ffe07466816246b8c6d478dfc5df45644d6bb
jeyefendi <jeyhunfarajov@gmail.com>
2022-09-11 15:06:40 +0300

    step 3 completed

--- wp-content/themes/childhood/header.php
+++ wp-content/themes/childhood/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title><?php bloginfo('name'); echo " | "; bloginfo('description')?></title>
+		<title><?php bloginfo('name'); echo " | "; bloginfo('description'); ?></title>
         <meta name="theme-color" content="#c9e0e04d">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -16,9 +16,8 @@
         <link rel="apple-touch-icon" sizes="120x120" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-120x120.png">
         <link rel="apple-touch-icon" sizes="144x144" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-144x144.png">
         <link rel="apple-touch-icon" sizes="180x180" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-180x180.png">
-        <link rel="stylesheet" href="./styles/main.min.css">
-        <?php
-            wp_head();        
+        <?php 
+            wp_head();
         ?>
     </head>
     <body class="no-svg">
@@ -26,12 +25,9 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-11 col-sm-12 col-md-12 col-lg-3">
-                        <a href="<?php echo get_home_url(); ?>" class="header__logo">
-                            <img src="<?php
-                            $custom_logo__url = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' ); 
-                            echo $custom_logo__url[0]; 
-                            ?>" alt="Мир детства" class="header__logo-img">
-                        </a>
+                        <div class="header__logo">
+                            <?php the_custom_logo(); ?>
+                        </div>
                     </div>
                     <div class="col-md-12 col-lg-9 offset-lg-0 col-xl-7 offset-xl-2 ">
                         <div class="header__contacts">
@@ -42,14 +38,13 @@
                             <div class="header__contacts-item">
                                 <img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/svg/phone.svg" alt="телефон" class="header__contacts-logo">
                                 <div class="header__contacts-tel">
-                                    <a href="tel:+797867834347">+797867834347</a>
-                                    <a href="tel:+797867834358">+797867834358</a>
+                                    <a href="tel:<?php the_field('phone_1', 2); ?>"><?php the_field('phone_1', 2); ?></a>
+                                    <a href="tel:<?php the_field('phone_2', 2); ?>"><?php the_field('phone_2', 2); ?></a>
                                 </div>
                             </div>
                             <div class="header__contacts-item">
                                 <img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/svg/pointer.svg" alt="указатель" class="header__contacts-logo">
-                                <address>ул. Василисы Кожиной, 1<br>
-                                    Москва, Россия, 121096</address>
+                                <address><?php the_field('addr', 2); ?></address>
                             </div>
                         </div>
                     </div>
